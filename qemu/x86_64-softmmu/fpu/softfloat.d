0963f0919dcd0be97c862443b10dab48b51b15e0
linux_parallels <h0415am@163.com>
2022-02-15 10:46:11 +0800

    linux commit_3

---
+++ qemu/x86_64-softmmu/fpu/softfloat.d
@@ -0,0 +1,21 @@
+fpu/softfloat.o: /home/parallels/6.828/qemu/fpu/softfloat.c \
+ /home/parallels/6.828/qemu/include/config.h \
+ /home/parallels/6.828/qemu/config-host.h config-target.h \
+ /home/parallels/6.828/qemu/include/fpu/softfloat.h \
+ /home/parallels/6.828/qemu/include/qemu/osdep.h \
+ /home/parallels/6.828/qemu/fpu/softfloat-macros.h \
+ /home/parallels/6.828/qemu/fpu/softfloat-specialize.h
+
+/home/parallels/6.828/qemu/include/config.h:
+
+/home/parallels/6.828/qemu/config-host.h:
+
+config-target.h:
+
+/home/parallels/6.828/qemu/include/fpu/softfloat.h:
+
+/home/parallels/6.828/qemu/include/qemu/osdep.h:
+
+/home/parallels/6.828/qemu/fpu/softfloat-macros.h:
+
+/home/parallels/6.828/qemu/fpu/softfloat-specialize.h:
